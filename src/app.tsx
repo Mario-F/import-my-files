026f8f6ced6dc4f27f5462edb1fbaf6f6c8b19f9
Mario-F <github@fritschen.net>
2021-10-11 19:32:57 +0200

    feat: add logging

--- src/app.tsx
+++ src/app.tsx
@@ -4,11 +4,10 @@ import * as ReactDOM from 'react-dom'
 import { IConfigTestHomedir } from './shared/config/test'
 
 function render() {
+  const configTest: IConfigTestHomedir = ipcRenderer.sendSync('config-test', 'homedir')
+  // eslint-disable-next-line no-console
+  console.log(configTest)
   ReactDOM.render(<h2>Hello from React!</h2>, document.body)
 }
-
-const configTest: IConfigTestHomedir = ipcRenderer.sendSync('config-test', 'homedir')
-// eslint-disable-next-line no-console
-console.log(configTest)
 
 render()
--- src/config/test.ts
+++ src/config/test.ts
@@ -1,10 +1,14 @@
 import { app, ipcMain } from 'electron'
+import log from 'electron-log'
 import jetpack from 'fs-jetpack'
-import { IConfigTestHomedir } from 'src/shared/config/test'
+import { IConfigTestHomedir } from '../shared/config/test'
 
 ipcMain.on('config-test', (event, arg) => {
+  log.verbose(`ipc config-test called with arg ${arg}`)
+
   if (arg === 'homedir') {
-    const homedirFiles = jetpack.list(app.getPath('home'))
-    event.returnValue = <IConfigTestHomedir>homedirFiles
+    const homedirFiles: IConfigTestHomedir = { files: jetpack.list(app.getPath('home')) }
+    log.verbose(`found ${homedirFiles.files.length} files in homedir`)
+    event.returnValue = homedirFiles
   }
 })
